3795b837a5916fab54428d81d070f890f02dd1ed
hassanShakur <hass.shakurhassan@gmail.com>
2023-01-13 21:38:18 +0300

    eslint config trial

--- 07-Natours/app.js
+++ 07-Natours/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Middleware called 👋');
+  console.log('Middleware called');
   req.timeRequested = new Date().toISOString();
   next();
 });
