9e00ca3aba6ff0fd231759332f5789e4fe5f3667
hassanShakur <hass.shakurhassan@gmail.com>
2023-01-14 16:59:07 +0300

    Success db connection

--- 07-Natours/server.js
+++ 07-Natours/server.js
@@ -1,7 +1,25 @@
+/* eslint-disable node/no-extraneous-require */
 const dotenv = require('dotenv');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require('mongoose');
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.ENCODED_DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then((con) => {
+    console.log(con.connection);
+    console.log('DB connection successful...');
+  });
 
 // console.log(app.get('env'));
 // console.log(process.env);
