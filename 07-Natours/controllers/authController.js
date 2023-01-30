e172aef2375a8f0f17ed3498b40912a547ab4d36
hassanShakur <hass.shakurhassan@gmail.com>
2023-01-30 19:21:31 +0300

    Start auth

--- 07-Natours/controllers/authController.js
+++ 07-Natours/controllers/authController.js
@@ -1,11 +1,22 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
